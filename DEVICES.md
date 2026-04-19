ece74433e9c9fd01d4ed15c943dce9ccc5495865
Jeroen Hof <32430809+Metal-Eagle@users.noreply.github.com>
2026-04-19 08:17:11 +0200

    Update

--- DEVICES.md
+++ DEVICES.md
@@ -9,13 +9,37 @@
 |                      | Air conditioner          | 008              | 304                    |
 | AWUS1225TW           | Air conditioner          | 008              | 399                    |
 |                      | Air conditioner          | 009              | 100                    |
+|                      | Air conditioner          | 009              | 101                    |
+|                      | Air conditioner          | 009              | 102                    |
+|                      | Air conditioner          | 009              | 103                    |
 |                      | Air conditioner          | 009              | 104                    |
 | AS-09TW2RLDTT00      | Air conditioner          | 009              | 105                    |
 |                      | Air conditioner          | 009              | 106                    |
+|                      | Air conditioner          | 009              | 107                    |
+|                      | Air conditioner          | 009              | 108                    |
 |                      | Air conditioner          | 009              | 109                    |
+|                      | Air conditioner          | 009              | 110                    |
+|                      | Air conditioner          | 009              | 111                    |
+|                      | Air conditioner          | 009              | 112                    |
+|                      | Air conditioner          | 009              | 113                    |
+|                      | Air conditioner          | 009              | 114                    |
+|                      | Air conditioner          | 009              | 115                    |
+|                      | Air conditioner          | 009              | 116                    |
 |                      | Air conditioner          | 009              | 117                    |
+|                      | Air conditioner          | 009              | 118                    |
+|                      | Air conditioner          | 009              | 119                    |
+|                      | Air conditioner          | 009              | 120                    |
+|                      | Air conditioner          | 009              | 121                    |
+|                      | Air conditioner          | 009              | 122                    |
+|                      | Air conditioner          | 009              | 123                    |
+|                      | Air conditioner          | 009              | 124                    |
+|                      | Air conditioner          | 009              | 125                    |
+|                      | Air conditioner          | 009              | 126                    |
+|                      | Air conditioner          | 009              | 127                    |
 |                      | Air conditioner          | 009              | 128                    |
 |                      | Air conditioner          | 009              | 129                    |
+|                      | Air conditioner          | 009              | 199                    |
+|                      | Air conditioner          | 009              | 19901                  |
 |                      | Hood                     | 012              | 000                    |
 |                      | Oven                     | 013              | 000                    |
 | Bio21                | Oven                     | 013              | oven-bio21-iconledplus |
@@ -31,14 +55,18 @@
 |                      | Washing machine          | 025              | 1wj090660v0w           |
 | WF5S9045BW           | Washing machine          | 025              | 1wj090728v0w           |
 | WD3S9043BB3          | Washing machine          | 025              | 1wj090913v0f           |
+| WF3S1014-SVW002      | Washing machine          | 025              | 1wj100404v0w           |
 | WFQR1014             | Washing machine          | 025              | 1wj100649v0t           |
 | WF3S1043BW3          | Washing machine          | 025              | 1wj100722v0w           |
 | HWFS1015AB           | Washing machine          | 025              | 1wj100923v0f           |
 |                      | Washing machine          | 025              | 1wj105050v0w           |
+| WFQA1014             | Washing machine          | 025              | 1wj105080v0w           |
+|                      | Washing machine          | 025              | 1wj105091v0t           |
 | WFSE1114-LVW002      | Washing machine          | 025              | 1wj105219v0w           |
 | WF3S1114-LVW004      | Washing machine          | 025              | 1wj105246v0w           |
 |                      | Washing machine          | 025              | 1wj105418v0t           |
 | WD5I1045BWQ          | Washing machine          | 025              | 1wj105552v0w           |
+| WF5S1245BB           | Washing machine          | 025              | 1wj120238v0b           |
 | WDSE1214-EVAJMW      | Washing machine          | 025              | 1wj120261v0w           |
 | WFSE1214-MVW002      | Washing machine          | 025              | 1wj120389v0b           |
 |                      | Washing machine          | 025              | 1wj120407v0w           |
@@ -57,5 +85,6 @@
 | DH3S802BW3           | Tumble dryer             | 030              | 1wk080066v0w           |
 | DH5S102BW            | Tumble dryer             | 030              | 1wk100028v0w           |
 | DHSE10               | Tumble dryer             | 030              | 1wk100130v0f           |
+| DH5S102BB            | Tumble dryer             | 030              | 1wk100266v0f           |
 | DHSE80-BEW001        | Tumble dryer             | 030              | 1wk080027e0w           |
 | DPNA83W              | Tumble dryer             | 032              | 000                    |
--- scripts/gen_strings.py
+++ scripts/gen_strings.py
@@ -1,13 +1,24 @@
+import argparse
 import json
+import re
+import urllib.request
 from os import listdir
 from os.path import isfile, join
 
 import yaml
 
+from scripts import check_translations, sort_translations
+
+HA_STRINGS_URL = "https://raw.githubusercontent.com/home-assistant/core/dev/homeassistant/strings.json"
+
 
 def main(basedir):
+    ha_strings = load_ha_strings()
     with open(f'{basedir}/strings.json', 'r') as f:
         strings = json.load(f)
+    valid_properties = {"sensor": {"daily_energy_kwh"}}
+    valid_options = {}
+
     device_dir = f'{basedir}/data_dictionaries'
     filenames = list(filter(lambda f: f[-5:] == ".yaml", [f for f in listdir(device_dir) if isfile(join(device_dir, f))]))
     for filename in filenames:
@@ -53,14 +64,34 @@ def main(basedir):
                     else:
                         if "disable" in property and property["disable"]:
                             continue
+                        key = to_key(property["property"])
+                        if not any(entity_type in property for entity_type in ["binary_sensor", "climate", "humidifier", "number", "select", "sensor", "switch", "water_heater"]):
+                            valid_properties.setdefault("sensor", set()).add(key)
+                            name = property["property"]
+                            if "sensor" not in strings["entity"]:
+                                strings["entity"]["sensor"] = {}
+                            if key not in strings["entity"]["sensor"]:
+                                strings["entity"]["sensor"][key] = {"name": pretty(name)}
+                            elif "name" not in strings["entity"]["sensor"][key]:
+                                strings["entity"]["sensor"][key]["name"] = pretty(name)
                         for entity_type in ["binary_sensor", "switch", "number", "sensor", "select"]:
                             if entity_type in property:
                                 if entity_type not in strings["entity"]:
                                     strings["entity"][entity_type] = {}
                                 name = property["property"]
-                                key = name.lower().replace(" ", "_")
+                                key = to_key(name)
                                 if key not in strings["entity"][entity_type]:
                                     strings["entity"][entity_type][key] = {"name": pretty(name)}
+                                elif "name" not in strings["entity"][entity_type][key]:
+                                    strings["entity"][entity_type][key]["name"] = pretty(name)
+                                valid_properties.setdefault(entity_type, set()).add(key)
+                                if (
+                                        property[entity_type] is not None
+                                        and "options" in property[entity_type]
+                                        and property[entity_type]["options"] is not None
+                                ):
+                                    for option in property[entity_type]["options"].values():
+                                        valid_options.setdefault((entity_type, key), set()).add(option)
                                 if (
                                         (
                                                 (
@@ -95,12 +126,90 @@ def main(basedir):
                             if include_option(preset, filename):
                                 strings["entity"]["climate"]["connectlife"]["state_attributes"]["preset_mode"]["state"][preset] = pretty(preset)
 
+    for entity_type in ["binary_sensor", "switch", "number", "sensor", "select"]:
+        if entity_type not in strings["entity"]:
+            continue
+        valid = valid_properties.get(entity_type, set())
+        for key in list(strings["entity"][entity_type]):
+            if key not in valid:
+                print(f"Removing stale {entity_type}.{key}")
+                del strings["entity"][entity_type][key]
+            elif "state" in strings["entity"][entity_type][key]:
+                valid_opts = valid_options.get((entity_type, key), set())
+                for option in list(strings["entity"][entity_type][key]["state"]):
+                    if option not in valid_opts:
+                        print(f"Removing stale {entity_type}.{key}.state.{option}")
+                        del strings["entity"][entity_type][key]["state"][option]
+                if not strings["entity"][entity_type][key]["state"]:
+                    del strings["entity"][entity_type][key]["state"]
+
     for (k, v) in strings["entity"].items():
         strings["entity"][k] = dict(sorted(v.items()))
 
     with open(f'{basedir}/strings.json', 'w') as f:
-        json.dump(strings, f, indent=2)
+        json.dump(strings, f, indent=2, sort_keys=True)
         f.write("\n")
+
+    en = expand_keys(strings, ha_strings)
+    with open(f'{basedir}/translations/en.json', 'w') as f:
+        json.dump(en, f, indent=2, sort_keys=True)
+        f.write("\n")
+
+    prune_translations(basedir, en)
+    sort_translations.main(basedir)
+
+def prune_translations(basedir, reference):
+    translations_dir = f'{basedir}/translations'
+    for filename in sorted(listdir(translations_dir)):
+        if filename.endswith('.json') and filename != 'en.json':
+            filepath = join(translations_dir, filename)
+            with open(filepath, 'r') as f:
+                trans = json.load(f)
+            pruned = prune_keys(trans, reference)
+            with open(filepath, 'w') as f:
+                json.dump(pruned, f, indent=2, ensure_ascii=True, sort_keys=True)
+                f.write("\n")
+
+
+def prune_keys(obj, reference):
+    if not isinstance(obj, dict) or not isinstance(reference, dict):
+        return obj
+    return {k: prune_keys(v, reference[k]) for k, v in obj.items() if k in reference}
+
+
+def load_ha_strings():
+    print(f"Fetching HA strings from {HA_STRINGS_URL}")
+    try:
+        with urllib.request.urlopen(HA_STRINGS_URL, timeout=30) as r:
+            return json.load(r)
+    except Exception as e:
+        print(f"Failed to fetch HA strings: {e}")
+        exit(1)
+
+
+def resolve_key(ha_strings, key):
+    """Resolve a key like 'common::config_flow::data::username' against HA strings."""
+    parts = key.split("::")
+    obj = ha_strings
+    for part in parts:
+        if not isinstance(obj, dict) or part not in obj:
+            return None
+        obj = obj[part]
+    return obj
+
+
+def expand_keys(obj, ha_strings):
+    if isinstance(obj, dict):
+        return {k: expand_keys(v, ha_strings) for k, v in obj.items()}
+    if isinstance(obj, str) and obj.startswith("[%key:") and obj.endswith("%]"):
+        key = obj[6:-2]
+        value = resolve_key(ha_strings, key)
+        if value is None:
+            print(f"Unknown HA common string: {key}")
+            exit(1)
+        return value
+    return obj
+
 
 def is_number(s: str) -> bool:
     try:
@@ -121,9 +230,27 @@ def include_option(option: str, filename: str) -> bool:
     return True
 
 
+def to_key(name: str) -> str:
+    return re.sub(r'_+', '_', name.strip().lower().replace(" ", "_"))
+
+
 def pretty(name: str) -> str:
-    return name.replace("_", " ").capitalize();
+    # Split camelCase: "AirDryFlag" -> "Air Dry Flag"
+    name = re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', name)
+    # Split acronyms from words: "APPControl" -> "APP Control"
+    name = re.sub(r'(?<=[A-Z])(?=[A-Z][a-z])', ' ', name)
+    # Split letters from digits: "compartment1" -> "compartment 1", "1add" -> "1 add"
+    name = re.sub(r'(?<=[a-zA-Z])(?=\d)', ' ', name)
+    name = re.sub(r'(?<=\d)(?=[a-zA-Z])', ' ', name)
+    return re.sub(r' +', ' ', name.replace("_", " ")).strip().capitalize()
 
 
 if __name__ == "__main__":
-    main("custom_components/connectlife")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--show-missing", nargs="?", const="", default=None,
+                        metavar="LANG", help="show missing translation keys (optionally for a specific language, e.g. nl)")
+    args = parser.parse_args()
+    basedir = "custom_components/connectlife"
+    main(basedir)
+    if args.show_missing is not None:
+        check_translations.main(basedir, lang=args.show_missing)
